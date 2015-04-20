5d1f0a914766166f406f74b8c5de08b74841c0de
timurvafin <timur.vafin@flatstack.com>
2015-04-20 13:26:46 +0300

    Load git completion for `g` alias

--- git/completion.bash
+++ git/completion.bash
@@ -2,5 +2,9 @@ completion="$(brew --prefix)/etc/bash_completion.d/git-completion.bash"
 
 if test -f "$completion"
 then
+  # load git completion
   source "$completion"
+
+  # set completion for `g` alias
+  __git_complete g __git_main
 fi
