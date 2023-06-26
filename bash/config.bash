c932c63cb5eebb95375c138722c30e8d32a1c9cb
timurvafin <timur.vafin@flatstack.com>
2023-06-26 14:17:41 +0300

    Use arm64 and vscode

--- bash/config.bash
+++ bash/config.bash
@@ -3,11 +3,11 @@ bind "set completion-ignore-case on"
 bind '"\e[A": history-search-backward'
 bind '"\e[B": history-search-forward'
 
-export ARCHFLAGS='-arch x86_64'
+export ARCHFLAGS='-arch arm64'
 
 export LC_CTYPE=en_US.UTF-8
 
-export VISUAL="subl"
+export VISUAL="code"
 export EDITOR="$VISUAL"
 export LESS="FRX"
 export RI="--format ansi -T"
